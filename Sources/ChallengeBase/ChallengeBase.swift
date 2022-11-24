7240baa67d169ffa3a415e10dd06b577427e605a
Radamés <r.alfaro8@icloud.com>
2022-11-24 19:18:54 -0400

    Updating text lines
    Testing out why code won't work when imported as a package

--- Sources/ChallengeBase/ChallengeBase.swift
+++ Sources/ChallengeBase/ChallengeBase.swift
@@ -1,5 +1,5 @@
 public struct ChallengeBase {
-    public private(set) var text = "Hello, World!"
+    public private(set) var text = "Hello, ChallengeBase!"
 
     public init() {
     }
--- Tests/ChallengeBaseTests/ChallengeBaseTests.swift
+++ Tests/ChallengeBaseTests/ChallengeBaseTests.swift
@@ -6,6 +6,6 @@ final class ChallengeBaseTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ChallengeBase().text, "Hello, World!")
+        XCTAssertEqual(ChallengeBase().text, "Hello, ChallengeBase!")
     }
 }
