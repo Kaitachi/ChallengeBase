f22a09924eab424c2b1fb44f060373f55058692a
Radamés <r.alfaro8@icloud.com>
2022-11-25 15:37:21 -0400

    Adding numeric 2D Array utility methods

--- Sources/ChallengeBase/Utilities/String+Extensions.swift
+++ Sources/ChallengeBase/Utilities/String+Extensions.swift
@@ -8,13 +8,31 @@
 import Foundation
 
 public extension String {
-    func integerList(separatedBy: CharacterSet = .newlines) -> [Int] {
-        return self.components(separatedBy: separatedBy)
+    func integerList(separatedBy itemSeparator: CharacterSet = .newlines) -> [Int] {
+        return self.components(separatedBy: itemSeparator)
             .compactMap { Int($0) }
     }
     
-    func doubleList(separatedBy: CharacterSet = .newlines) -> [Double] {
-        return self.components(separatedBy: separatedBy)
+    func integerArray2D(separatedBy rowSeparator: CharacterSet = .newlines, thenBy columnSeparator: CharacterSet = .whitespaces) -> [[Int]] {
+        return self.components(separatedBy: rowSeparator)
+            .filter { !$0.isEmpty }
+            .compactMap { line in
+                line.components(separatedBy: columnSeparator)
+                    .compactMap { cell in Int(cell) }
+            }
+    }
+    
+    func doubleList(separatedBy itemSeparator: CharacterSet = .newlines) -> [Double] {
+        return self.components(separatedBy: itemSeparator)
             .compactMap { Double($0) }
+    }
+    
+    func doubleArray2D(separatedBy rowSeparator: CharacterSet = .newlines, thenBy columnSeparator: CharacterSet = .whitespaces) -> [[Double]] {
+        return self.components(separatedBy: rowSeparator)
+            .filter { !$0.isEmpty }
+            .compactMap { line in
+                line.components(separatedBy: columnSeparator)
+                    .compactMap { cell in Double(cell) }
+            }
     }
 }
